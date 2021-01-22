494406864a2a0123584115c7781329fdf156fe49
João Pedro Alves <jotalmeida007@hotmail.com>
2021-01-22 20:47:27 -0300

    Add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Akali from '../../assets/Akali.jpeg';
+import { CgMenuGridR } from 'react-icons/cg';
 
 import './header.css';
 
 export default function Header() {
   return (
     <div className="headerContainer">
-      <a>Gmail</a>
-      <a>Imagens</a>
-      {/* Box icons */}
-      <img src={Akali} alt="" />
+      <a id="gmail">Gmail</a>
+      <a id="img">Imagens</a>
+      <CgMenuGridR id="gridIcon" size="30" />
+      <div className="userIcon" />
     </div>
   );
 }
